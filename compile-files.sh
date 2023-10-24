00a1c4cbd75fcf53d3ad8535f46c5ccb39acea6c
Tim <tim@zolleis.net>
2023-10-24 11:12:33 +0200

    chore(comments): remove unnecessary comments

--- compile-files.sh
+++ compile-files.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Define the directory to search for .tex files
 directory="/tasks"
 
-# Use 'find' to locate all .tex files in the specified directory and subdirectories
 find "$directory" -type f -name "*.tex" -print0 | while IFS= read -r -d '' file; do
-    # Get the directory containing the .tex file
     dir="$(dirname "$file")"
 
-    # Change to the directory
     cd "$dir" || exit 1
 
     mkdir -p out
     echo "Compiling $file..."
     SECONDS=0
-    # Compile the .tex file to PDF using 'pdflatex'
+    # This command runs two times to ensure references are correct
     pdflatex -interaction=batchmode -output-directory out "$(basename "$file")"
-    # Return to the original directory
+    pdflatex -interaction=batchmode -output-directory out "$(basename "$file")"
     echo "Successfully compiled $file - took $SECONDS seconds"
     cd - || exit 1
 done
