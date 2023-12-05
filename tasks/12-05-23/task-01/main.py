c2afc4dbeecfe3e8af697f3428ffd32655a34c34
timzolleis <tim@zolleis.net>
2023-12-05 09:09:17 +0100

    feat(tasks): add tasks of 12-05-23

---
+++ tasks/12-05-23/task-01/main.py
@@ -0,0 +1,23 @@
+def task1(a, b):
+    return not a and not b
+
+def task2(a, c):
+    return a or not c
+
+def task3():
+    return False
+
+def task4(a):
+    return a
+
+def task5(a ,b):
+    return a and not b
+
+def task6(a, c):
+    return a and c
+
+def task7(a, b, c):
+    return not (a and b) or not c
+
+def task8(a, b, c):
+    return (a and b) and not c
